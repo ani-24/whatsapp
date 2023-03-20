4b03407bb1ee8caba3adc20bbe24f59328f85739
Aniket <rajeshwarkumar9204@gmail.com>
2023-03-20 08:48:54 +0530

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.send("Hello, world");
 });
 
 app.listen(port, () => {
